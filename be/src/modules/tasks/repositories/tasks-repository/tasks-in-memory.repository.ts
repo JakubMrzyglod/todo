2c834dfb21eb90c3ea369bbba936fa9e279be3c9
Jakub Mrzyglod <jakub1mrzyglod@gmail.com>
2023-08-23 09:39:52 +0200

    create method for tasks in memroy repo

--- be/src/modules/tasks/repositories/tasks-repository/tasks-in-memory.repository.ts
+++ be/src/modules/tasks/repositories/tasks-repository/tasks-in-memory.repository.ts
@@ -4,8 +4,13 @@ import { ICreateTask, ITask } from '../../interfaces';
 
 @Injectable()
 export class TasksInMemoryRepository implements TasksRepository {
+  private tasks: ITask[] = [];
+
   create(taskDetails: ICreateTask): Promise<ITask> {
-    throw new Error('Method not implemented.');
+    const id = this.tasks.length + 1;
+    const newTasks = { ...taskDetails, id, done: false };
+    this.tasks.push(newTasks);
+    return Promise.resolve(newTasks);
   }
   getAll(): Promise<ITask[]> {
     throw new Error('Method not implemented.');
