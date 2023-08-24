030d9b0d98f0e9b1d20c1abc99259ec151e8eb09
Jakub Mrzyglod <jakub1mrzyglod@gmail.com>
2023-08-24 10:56:28 +0200

    fix paths

--- fe/src/tasks/organisms/add-tasks-form.tsx
+++ fe/src/tasks/organisms/add-tasks-form.tsx
@@ -1,7 +1,16 @@
+import { Input } from '@components';
+
 export const AddTaskForm = () => (
   <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
     <div className="w-full">
       <form className="flex items-center">
+        <Input
+          {...{
+            name: 'content',
+            className:
+              'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
+          }}
+        />
         <input
           type="text"
           id="simple-search"
